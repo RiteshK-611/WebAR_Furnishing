00498a092624ac13156d1a591b2b6543d4286090
Ritesh <riteshkokam@gmail.com>
2022-05-01 00:58:14 +0530

    package updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Cart from "./components/cart/Cart";
@@ -18,12 +18,12 @@ function App() {
         <BrowserRouter basename="/">
           <Header />
           <Box style={{ marginTop: 54 }}>
-            <Switch>
+            <Routes>
               <Route exact path="/" component={Home} />
               <Route exact path="/cart" component={Cart} />
               <Route exact path="/product/:id" component={DetailView} />
               <Route exact path="/product/ar/:id" component={ARView} />
-            </Switch>
+            </Routes>
           </Box>
           {/* <Footer />   */}
         </BrowserRouter>
