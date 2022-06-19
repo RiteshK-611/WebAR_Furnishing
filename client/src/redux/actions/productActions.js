29dc53bfe05f41e599bd1b70ba20ec93efa38ccd
Ritesh <riteshkokam@gmail.com>
2022-06-19 21:53:34 +0530

    rm /api

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -5,7 +5,7 @@ const url = "";
 
 export const getProducts = () => async (dispatch) => {
   try {
-    const { data } = await axios.get(`${url}/api/products`);
+    const { data } = await axios.get(`${url}/products`);
     dispatch({ type: actionTypes.GET_PRODUCTS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: actionTypes.GET_PRODUCTS_FAIL, payload: error.response });
@@ -14,7 +14,7 @@ export const getProducts = () => async (dispatch) => {
 
 export const getProductDetails = (id) => async (dispatch) => {
   try {
-    const { data } = await axios.get(`${url}/api/product/${id}`);
+    const { data } = await axios.get(`${url}/product/${id}`);
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,25 @@
-import express from 'express';
-import {userSignup, userLogIn } from '../controllers/userController.js';
-import { getProducts,  getProductById } from '../controllers/product-controller.js';
-import { addPaymentGateway, paymentResponse } from '../controllers/payment-controller.js';
+import express from "express";
+import { userSignup, userLogIn } from "../controllers/userController.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/product-controller.js";
+import {
+  addPaymentGateway,
+  paymentResponse,
+} from "../controllers/payment-controller.js";
 
 const router = express.Router();
 
-router.post('/api/signup', userSignup);
-router.post('/api/login', userLogIn);
+router.post("/signup", userSignup);
+router.post("/login", userLogIn);
 
-router.get('/api/products', getProducts);
-router.get('/api/product/:id', getProductById);
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
 
-router.post('/api/payment', addPaymentGateway);
-router.post('/api/callback', paymentResponse);
-
+router.post("/payment", addPaymentGateway);
+router.post("/callback", paymentResponse);
 
 //router.post('/addProduct', addNewProductToDb )
 
-
-
 export default router;
-
