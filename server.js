44a58fa76384fa0bc6a56bcf21fe9b0c26b799aa
Ritesh <riteshkokam@gmail.com>
2022-06-19 18:38:10 +0530

    update routes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/", Routers);
+app.use("/api", Routers);
 
 const PORT = process.env.PORT || 8000;
 // const PORT = 8000;
